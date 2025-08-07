f7644cefb77fec74478e392e3bba002256971dee
Szabikaa1820 <balogszabolcs528@gmail.com>
2025-08-07 03:40:05 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,27 @@
+
+{
+  "name": "cryptoApp",
+  "version": "1.0.0",
+  "main": "node_modules/expo/AppEntry.js",
+  "scripts": {
+    "start": "expo start"
+  },
+  "dependencies": {
+    "expo": "~50.0.3",
+    "expo-blur": "~12.0.1",
+    "expo-linear-gradient": "~12.1.2",
+    "lucide-react-native": "^0.264.0",
+    "react": "18.2.0",
+    "react-native": "0.73.6",
+    "react-native-svg": "15.1.1"
+  },
+  "private": true
+}
+
+
+
+
+
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
